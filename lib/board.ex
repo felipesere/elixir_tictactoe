e0e9d60f4c43154f3739ce5d3e8e90f08f339175
fabien townsend <fabientownsend@gmail.com>
2017-04-03 12:11:38 +0100

    Initialization module board

---
+++ lib/board.ex
@@ -0,0 +1,87 @@
+defmodule Board do
+  def empty_board do
+    List.to_tuple(create_board())
+  end
+
+  defp create_board do
+    Stream.repeatedly(fn -> List.to_tuple(create_row()) end)
+    |> Enum.take(3)
+  end
+
+  defp create_row do
+    Stream.repeatedly(fn -> " " end)
+    |> Enum.take(3)
+  end
+
+  def set_mark(board, mark, position) do
+    row_index = row(board, position)
+    column_index = column(board, position)
+
+    updapted_row =
+      board
+      |> elem(row_index)
+      |> put_elem(column_index, mark)
+
+    board
+    |> put_elem(row_index, updapted_row)
+  end
+
+  def is_free(board, position) do
+    row_index = row(board, position)
+    column_index = column(board, position)
+
+    board
+    |> elem(row_index)
+    |> elem(column_index) == " "
+  end
+
+  def row(board, position) do
+    round(position / tuple_size(board))
+  end
+
+  def column(board, position) do
+    rem(position, tuple_size(board))
+  end
+
+  def win(board, mark) do
+    any_turple(Tuple.to_list(board), mark)
+  end
+
+  def any_turple(board, mark) do
+    board
+    |> Enum.any?(fn(x) -> all_turple(x, mark) end)
+  end
+
+  def all_turple(row, mark) do
+    Tuple.to_list(row)
+    |> Enum.all?(fn(x) -> x == mark end)
+  end
+
+  def check_columns(board, mark) do
+    any_turple(List.zip(Tuple.to_list(board)), mark)
+  end
+
+  def check_diag_one(board, mark) do
+    0..2
+    |> Enum.all?(fn(x) -> board |> elem(x) |> elem(x) == mark end)
+  end
+
+  def check_diag_two(board, mark) do
+    0..2
+    |> Enum.all?(fn(x) -> board |> elem(x) |> elem(tuple_size(board) - 1 - x) == mark end)
+  end
+
+  def available(board) do
+    Tuple.to_list(board)
+    |> available(tuple_size(board) - 1)
+  end
+
+  def available([], _), do: []
+  def available([head | tail], index) do
+    if head == " " do
+      available(tail, index) ++ [index - Enum.count(tail)]
+    else
+      available(tail, index)
+    end
+  end
+end
---
+++ test/board_test.exs
@@ -0,0 +1,182 @@
+defmodule BoardTest do
+  use ExUnit.Case
+
+  test "returns an empty board by default" do
+    empty_board = {
+      {" ", " ", " "},
+      {" ", " ", " "},
+      {" ", " ", " "}
+    }
+
+    assert Board.empty_board == empty_board
+  end
+
+  test "returns a board with a mark" do
+    board = {
+      {" ", " ", " "},
+      {" ", " ", " "},
+      {" ", " ", " "}
+    }
+
+    marked_board = {
+      {" ", " ", " "},
+      {" ", "X", " "},
+      {" ", " ", " "}
+    }
+
+    assert Board.set_mark(board, "X", 4) == marked_board
+  end
+
+  test "returns board with mark changed" do
+    board = {
+      {" ", " ", " "},
+      {" ", "X", " "},
+      {" ", " ", " "}
+    }
+
+    marked_board = {
+      {" ", " ", " "},
+      {" ", "O", " "},
+      {" ", " ", " "}
+    }
+
+    assert Board.set_mark(board, "O", 4) == marked_board
+  end
+
+  test "returns true if the placeholder is free" do
+    board = {
+      {" ", " ", " "},
+      {" ", "O", " "},
+      {" ", " ", " "}
+    }
+
+    assert Board.is_free(board, 0) == true
+  end
+
+  test "returns false if the placeholder isn't free" do
+    board = {
+      {" ", " ", " "},
+      {" ", "O", " "},
+      {" ", " ", " "}
+    }
+
+    assert Board.is_free(board, 4) == false
+  end
+
+  test "returns false when no one win" do
+    board = {
+      {" ", " ", " "},
+      {" ", " ", " "},
+      {" ", " ", " "}
+    }
+
+    assert Board.win(board, "X") == false
+  end
+
+  test "returns true when first row win" do
+    board = {
+      {"X", "X", "X"},
+      {" ", " ", " "},
+      {" ", " ", " "}
+    }
+
+    assert Board.win(board, "X") == true
+  end
+
+  test "returns true when second row win" do
+    board = {
+      {" ", " ", " "},
+      {"X", "X", "X"},
+      {" ", " ", " "}
+    }
+
+    assert Board.win(board, "X") == true
+  end
+
+  test "returns true when third row win" do
+    board = {
+      {" ", " ", " "},
+      {" ", " ", " "},
+      {"X", "X", "X"}
+    }
+
+    assert Board.win(board, "X") == true
+  end
+
+  test "returns true when first column win" do
+    board = {
+      {"X", " ", " "},
+      {"X", " ", " "},
+      {"X", " ", " "}
+    }
+
+    assert Board.check_columns(board, "X") == true
+  end
+
+  test "return true when second column win" do
+    board = {
+      {" ", "X", " "},
+      {" ", "X", " "},
+      {" ", "X", " "}
+    }
+
+    assert Board.check_columns(board, "X") == true
+  end
+
+  test "return true when third column win" do
+    board = {
+      {" ", " ", "X"},
+      {" ", " ", "X"},
+      {" ", " ", "X"}
+    }
+
+    assert Board.check_columns(board, "X") == true
+  end
+
+  test "returns true when win first diag" do
+    board = {
+      {"X", " ", " "},
+      {" ", "X", " "},
+      {" ", " ", "X"}
+    }
+
+    assert Board.check_diag_one(board, "X") == true
+  end
+
+  test "returns true when win second diag" do
+    board = {
+      {" ", " ", "X"},
+      {" ", "X", " "},
+      {"X", " ", " "}
+    }
+
+    assert Board.check_diag_two(board, "X") == true
+  end
+
+  test "returns spot available" do
+    board = {"O", " ", " "}
+
+    assert Board.available(board) == [2, 1]
+  end
+
+ test "second" do
+   board = {"O", "O", " "}
+
+   assert Board.available(board) == [2]
+ end
+
+ test "full" do
+   board = {"O", "O", "O"}
+
+   assert Board.available(board) == []
+ end
+
+ test "version two" do
+   board = {" ", " ", " "}
+
+   assert Board.available(board) == [2, 1, 0]
+ end
+
+# test tie
+# return game over if game over
+end

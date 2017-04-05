877e59ab52c14424b01eb68323f7a43b982d8393
fabien townsend <fabientownsend@gmail.com>
2017-04-05 11:02:57 +0100

    Add minimax

--- lib/computer.ex
+++ lib/computer.ex
@@ -3,6 +3,10 @@ defmodule Computer do
   def other_mark("O"), do: "X"
 
   def minimax(board, mark) do
+    minimax(board, mark, false)
+  end
+
+  def minimax(board, mark, computer) do
     cond do
       board |> Board.win?(mark) ->
         1
@@ -10,6 +14,52 @@ defmodule Computer do
         - 1
       board |> Board.tie? ->
         0
+      true ->
+        if computer do
+          maximizing_player(board, mark)
+        else
+          minimizing_player(board, mark)
+        end
+    end
+  end
+
+  def maximizing_player(board, mark) do
+    spots = board |> Board.available_placeholders
+    best_value = spots |> Enum.map(
+      fn(x) ->
+        new_board = Board.set_mark(board, mark, x)
+        minimax(new_board, mark, false)
+      end
+    )
+
+    Enum.max(best_value)
+  end
+
+  def minimizing_player(board, mark) do
+    spots = board |> Board.available_placeholders
+    best_value = spots |> Enum.map(
+      fn(x) ->
+        new_board = Board.set_mark(board, other_mark(mark), x)
+        minimax(new_board, mark, true)
+      end
+    )
+
+    Enum.min(best_value)
+  end
+
+  def maxy(value_one, value_two) do
+    if (value_one > value_two) do
+      value_one
+    else
+      value_two
+    end
+  end
+
+  def miny(value_one, value_two) do
+    if (value_one > value_two) do
+      value_two
+    else
+      value_one
     end
   end
 end
--- test/computer_test.exs
+++ test/computer_test.exs
@@ -5,25 +5,62 @@ defmodule ComputerTest do
     assert Computer.other_mark("X") == "O"
   end
 
-  test "return X when give O" do
-    assert Computer.other_mark("O") == "X"
-  end
+ test "return X when give O" do
+   assert Computer.other_mark("O") == "X"
+ end
 
-  test "return 1 when computer win" do
-    board = [
-      ["X", "X", "X"],
-      [" ", " ", " "],
-      [" ", " ", " "]
-    ]
-    assert Computer.minimax(board, "X") == 1
-  end
+ test "return 1 when computer win" do
+   board = [
+     ["X", "X", "X"],
+     [" ", " ", " "],
+     [" ", " ", " "]
+   ]
+   assert Computer.minimax(board, "X") == 1
+ end
 
-  test "return -1 when computer win" do
-    board = [
-      ["O", "O", "O"],
-      [" ", " ", " "],
-      [" ", " ", " "]
-    ]
-    assert Computer.minimax(board, "X") == -1
-  end
+ test "return -1 when computer win" do
+   board = [
+     ["O", "O", "O"],
+     [" ", " ", " "],
+     [" ", " ", " "]
+   ]
+   assert Computer.minimax(board, "X") == -1
+ end
+
+ test "return 0 when tie" do
+   board = [
+     ["O", "O", "X"],
+     ["X", "X", "O"],
+     ["O", "X", "X"]
+   ]
+   assert Computer.minimax(board, "X") == 0
+ end
+
+ test "the opposite player will win" do
+   board = [
+     ["X", "X", "O"],
+     ["O", "O", "X"],
+     [" ", "X", "X"]
+   ]
+   assert Computer.minimax(board, "X") == -1
+ end
+
+ test "this will be a tie" do
+   board = [
+     ["X", "O", "X"],
+     ["O", "O", "X"],
+     [" ", "X", "O"]
+   ]
+   assert Computer.minimax(board, "X") == 0
+ end
+
+ test "lol" do
+   IO.puts "bugged test"
+   board = [
+     ["X", "X", "O"],
+     [" ", "O", " "],
+     ["X", "O", "X"]
+   ]
+   assert Computer.minimax(board, "X") == 0
+ end
 end

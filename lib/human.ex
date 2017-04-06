c18372d28a240079b94a799a0965c762854ad142
fabien townsend <fabientownsend@gmail.com>
2017-04-06 22:47:06 +0100

    Add better user experience for cli player

--- lib/human.ex
+++ lib/human.ex
@@ -1,6 +1,43 @@
 defmodule Human do
   def move(board, mark) do
-    {position, _} = Integer.parse(IO.gets "play")
-    position
+    read_input(mark)
+    |> extract_move
+    |> execute_move(board, mark)
+  end
+
+  defp read_input(mark) do
+    String.trim(IO.gets "Your move #{mark}: ")
+  end
+
+  defp extract_move(input) do
+    case Integer.parse(input) do
+      {position, ""} -> position
+      _ -> :bad_parse
+    end
+  end
+
+  defp execute_move(result, board, mark) do
+    case result do
+      :bad_parse -> request_valid_move(board, mark)
+      position -> play_move(board, position, mark)
+    end
+  end
+
+  defp request_valid_move(board, mark) do
+     IO.puts "You move must be an integer"
+     move(board, mark)
+  end
+
+  defp play_move(board, position, mark) do
+    if Board.is_free?(board, position) do
+      position
+    else
+      request_free_position(board, mark)
+    end
+  end
+
+  defp request_free_position(board, mark) do
+    IO.puts "The position must be free"
+    move(board, mark)
   end
 end
--- test/human_test.exs
+++ test/human_test.exs
@@ -1,7 +1,66 @@
 defmodule HumanTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
 
-  test "need test" do
-    assert true
+  test "returns a valid move" do
+    board = Board.empty_board
+    capture_io([input: "1\n"],
+      fn ->
+        assert Human.move(board, :x) == 1
+      end
+    )
+  end
+
+  test "requests twice when first input is a string" do
+    board = Board.empty_board
+    capture_io([input: "helloworld\n2\n"],
+      fn ->
+        assert Human.move(board, :x) == 2
+      end
+    )
+  end
+
+  test "requests twice when first input is a float" do
+    board = Board.empty_board
+    capture_io([input: "4.8\n3\n"],
+      fn ->
+        assert Human.move(board, :x) == 3
+      end
+    )
+  end
+
+  test "requests twice when first input mix string and integer" do
+    board = Board.empty_board
+    capture_io([input: "7asdf \n3\nasdf\n"],
+      fn ->
+        assert Human.move(board, :x) == 3
+      end
+    )
+  end
+
+  test "requests twice when first input is empty" do
+    board = Board.empty_board
+    capture_io([input: "\n4\n"],
+      fn ->
+        assert Human.move(board, :x) == 4
+      end
+    )
+  end
+
+  test "displays a erreor message when input is invalid" do
+    board = Board.empty_board
+    error_message = capture_io([input: "helloworld\n2\n"], fn -> Human.move(board, :x) end)
+
+    assert String.contains?(error_message, "You move must be an integer")
+  end
+
+  test "displays a message when the spot is free" do
+    board = [:x, 1, 2, 3, 4, 5, 6, 7, 8]
+
+    error_message = capture_io([input: "0\n1\n"],
+      fn -> Human.move(board, :x) end
+    )
+
+    assert String.contains?(error_message, "The position must be free\n")
   end
 end

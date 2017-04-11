a79b0668be872b2dc85f94371171403a044c9701
fabien townsend <fabientownsend@gmail.com>
2017-04-11 15:44:18 +0100

    Change method name

--- lib/cli.ex
+++ lib/cli.ex
@@ -3,7 +3,7 @@ defmodule Cli do
   def get_integer(request) do
     String.trim(IO.gets request)
     |> extract_integer
-    |> validate_move
+    |> validate_integer
   end
 
   defp extract_integer(input) do
@@ -13,6 +13,9 @@ defmodule Cli do
     end
   end
 
-  defp validate_move(:bad_parse), do: get_integer("The value must be an integer")
-  defp validate_move(move), do: move
+  defp validate_integer(:bad_parse) do
+    get_integer("The value must be an integer")
+  end
+
+  defp validate_integer(move), do: move
 end
